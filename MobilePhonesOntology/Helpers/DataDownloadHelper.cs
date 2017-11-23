2450c1e3d4cf8c8a39ccaff72e5688a1ba1f3aba
Paweł Maciejewski <paulomac1000@gmail.com>
2017-11-23 20:02:03 +0100

    download brands as brand class

--- MobilePhonesOntology/Helpers/DataDownloadHelper.cs
+++ MobilePhonesOntology/Helpers/DataDownloadHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
+using MobilePhonesOntology.Models;
 
 namespace MobilePhonesOntology.Helpers
 {
@@ -15,15 +16,22 @@ namespace MobilePhonesOntology.Helpers
             throw new NotImplementedException();
         }
 
-        public static IEnumerable<string> GetAllBrands()
+        public static IEnumerable<Brand> GetAllBrands()
         {
             using (var client = new WebClient())
             {
                 var htmlCode = client.DownloadString(BrandUrl);
 
-                const string regexPattern = "<a href=\"\\/brand\\/.{1,30}\">(?<brand>[^<]+)<\\/a>";
+                const string regexPattern = "<a href=\"\\/brand\\/(?<url>.{1,30})\">(?<brand>[^<]+)<\\/a>";
                 var matches = Regex.Matches(htmlCode, regexPattern, RegexOptions.Multiline);
-                var brands =  (from Match match in matches select match.Groups["brand"].Value).ToList();
+
+                var brands = from Match match in matches
+                    select new Brand
+                    {
+                        Id = Convert.ToInt32(Regex.Match(match.Groups["url"].Value, @"\d+").Value),
+                        Url = match.Groups["url"].Value,
+                        Name = match.Groups["brand"].Value
+                    };
 
                 return brands;
             }
