1cb21df4be5adb774940679ebf8e59d3e9deba82
Pawel Maciejewski <pmaciejewski@openskydata.com>
2017-12-18 17:47:07 +0100

    create cache helper

---
+++ MobilePhonesOntology/Helpers/CacheHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using VDS.RDF;
+
+namespace MobilePhonesOntology.Helpers
+{
+    public static class CacheHelper
+    {
+        private static Graph brandsAndModels { get; set; }
+        private static bool LockBrandsAndModels { get; set; } = false;
+
+        public static Graph BrandsAndModels
+        {
+            get
+            {
+                if (LockBrandsAndModels)
+                    throw new Exception("Wait when BrandsAndModels graph is updating.");
+                else
+                    return brandsAndModels;
+            }
+            set
+            {
+                LockBrandsAndModels = true;
+                brandsAndModels = value;
+                LockBrandsAndModels = false;
+            }
+        }
+
+        private static Graph phones { get; set; }
+        private static bool lockPhones { get; set; } = false;
+
+        public static Graph Phones
+        {
+            get
+            {
+                if (lockPhones)
+                    throw new Exception("Wait when Phones graph is updating.");
+                else
+                    return phones;
+            }
+            set
+            {
+                lockPhones = true;
+                phones = value;
+                lockPhones = false;
+            }
+        }
+    }
+}
